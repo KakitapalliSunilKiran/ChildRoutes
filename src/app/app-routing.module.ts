febe63362f5462c1e6867733be38d8d5197d2e58
KakitapalliSunilKiran <k.sunilkiran98@gmail.com>
2023-12-10 18:20:44 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { ChildAComponent } from './child-a/child-a.component';
+import { ChildBComponent } from './child-b/child-b.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+      path: 'first-component', 
+      component: FirstComponent,
+      children: [
+        {
+          path: 'child-a', // child route path
+          component: ChildAComponent, // child route component that the router renders
+        },
+        {
+          path: 'child-b',
+          component: ChildBComponent, // another child route component that the router renders
+        },
+      ],
+  },
+  { path: 'second-component', component: SecondComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
